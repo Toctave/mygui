c18eceb9b30a6a9729508d568942e6b49d0bb3d6
Octave <octave.crespel@club.fr>
2022-07-11 18:51:23 +0200

    Refactor stretchy buffer implementation

--- src/stretchy_buffer.c
+++ src/stretchy_buffer.c
@@ -15,19 +15,31 @@ void array_free_(mem_allocator_i* alloc, void* ptr, uint32_t element_size)
              sizeof(array_header_t) + element_size * header->capacity);
 }
 
-void* array_grow(mem_allocator_i* alloc, void* ptr, uint32_t element_size)
+void* array_reserve_(mem_allocator_i* alloc,
+                     void* ptr,
+                     uint32_t element_size,
+                     uint32_t required)
 {
+    if (!required)
+    {
+        return;
+    }
+
     if (!ptr)
     {
-        uint32_t initial_capacity = 16;
+        uint32_t min_capacity = 16;
+        if (required < min_capacity)
+        {
+            required = min_capacity;
+        }
+
         void* bytes =
-            mem_alloc(alloc,
-                      sizeof(array_header_t) + element_size * initial_capacity);
+            mem_alloc(alloc, sizeof(array_header_t) + element_size * required);
 
         void* result = ((uint8_t*)bytes + sizeof(array_header_t));
 
         array_header_t* header = bytes;
-        header->capacity = initial_capacity;
+        header->capacity = required;
         header->count = 0;
 
         return result;
@@ -36,18 +48,31 @@ void* array_grow(mem_allocator_i* alloc, void* ptr, uint32_t element_size)
     {
         array_header_t* old_header = array_header(ptr);
         uint32_t old_capacity = old_header->capacity;
+        ASSERT(old_capacity);
+
         uint32_t old_count = old_header->count;
 
-        uint32_t new_capacity = old_capacity * 2;
-        array_header_t* new_header =
-            mem_realloc(alloc,
-                        old_header,
-                        sizeof(array_header_t) + element_size * old_capacity,
-                        sizeof(array_header_t) + element_size * new_capacity);
+        if (required <= old_capacity)
+        {
+            return ptr;
+        }
+        else
+        {
+            uint32_t new_capacity = old_capacity;
+            while (new_capacity < required)
+            {
+                new_capacity = (new_capacity * 3) / 2;
+            }
+            array_header_t* new_header = mem_realloc(
+                alloc,
+                old_header,
+                sizeof(array_header_t) + element_size * old_capacity,
+                sizeof(array_header_t) + element_size * new_capacity);
 
-        new_header->count = old_count;
-        new_header->capacity = new_capacity;
+            new_header->count = old_count;
+            new_header->capacity = new_capacity;
 
-        return new_header + 1;
+            return new_header + 1;
+        }
     }
 }
--- src/stretchy_buffer.h
+++ src/stretchy_buffer.h
@@ -12,7 +12,10 @@ array_header_t* array_header(void* ptr);
 
 typedef struct mem_allocator_i mem_allocator_i;
 
-void* array_grow(mem_allocator_i* alloc, void* ptr, uint32_t element_size);
+void* array_reserve_(mem_allocator_i* alloc,
+                     void* ptr,
+                     uint32_t element_size,
+                     uint32_t required);
 
 void array_free_(mem_allocator_i* alloc, void* ptr, uint32_t element_size);
 
@@ -20,9 +23,14 @@ void array_free_(mem_allocator_i* alloc, void* ptr, uint32_t element_size);
 
 #define array_count(a) ((a) ? array_header(a)->count : 0)
 
-#define array_full(a)                                                          \
-    ((a) ? array_header(a)->count == array_header(a)->capacity : 1)
+#define array_reserve(alloc, a, capacity)                                      \
+    (a = array_reserve_(alloc, a, sizeof(*a), capacity))
 
 #define array_push(alloc, a, item)                                             \
-    array_full(a) ? a = array_grow(alloc, a, sizeof(*a)) : 0,                  \
-                    a[array_header(a)->count++] = item
+    do                                                                         \
+    {                                                                          \
+        array_reserve(alloc, a, array_count(a) + 1);                           \
+        a[array_header(a)->count++] = item;                                    \
+    } while (0)
+
+#define array_safe_get(a, index) (index < array_count(a) ? &a[index] : 0)
